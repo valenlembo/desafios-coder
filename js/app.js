25df71e1d19c50e2b5b7b964388076d3e9aa2483
Valen Lembo <valenlembo99@icloud.com>
2022-07-04 14:03:05 -0300

    todas las cards en el html

--- js/app.js
+++ js/app.js
@@ -1,108 +1,93 @@
 // variables globales
 let nombre = prompt('¿Cómo es tu nombre?')
-let card = document.getElementById("card")
+let totalCompra = document.getElementById("totalCompra")
+const carrito = []
 // obj constructor
 class Producto{
-    constructor(nombre, precio, categoria, id){
+    constructor(nombre, precio, foto, categoria, id){
         this.nombre = nombre
         this.precio = precio
+        this.foto = foto
         this.categoria = categoria
         this.id = id
     }
 }
 // OBJETOS DECO
-const almohadonesVarios = new Producto ("Almohadones Varios", 5600, 20, "deco", 001)
-const setMesaYBancos = new Producto ("Set Mesa y Bancos", 78999, 3, "deco", 002)
+const almohadonesVarios = new Producto ("Almohadones Varios", 5600, "./img/almohadones2.jpeg", "deco", 1)
+const setMesaYBancos = new Producto ("Set Mesa y Bancos", 78999, "./img/mesaConBancos.jpeg", "deco", 2)
 // OBJETOS CUADROS
-const cuadroVerde = new Producto ("Cuadro Verde", 5600, 15, "cuadros", 003)
-const cuadroBlanco = new Producto ("Cuadro Blanco", 8000, 7, "cuadros", 004)
+const cuadroVerde = new Producto ("Cuadro Verde", 5600, "./img/cuadro2.jpeg", "cuadros", 3)
+const cuadroBlanco = new Producto ("Cuadro Blanco", 8000, "./img/comoda.jpeg", "cuadros", 4)
 // OBJETOS VELAS
-const tallRibbedTower = new Producto ("Tall Ribbed Tower", 700, 20, "velas", 005)
-const shortRibbedTower = new Producto ("Short Ribbed Tower", 1000, 3, "velas", 006)
+const tallRibbedTower = new Producto ("Tall Ribbed Tower", 700, "./img/velas8.jpeg", "velas", 5)
+const shortRibbedTower = new Producto ("Short Ribbed Tower", 1000, "./img/velas12.jpeg", "velas", 6)
+// LISTA PRODUCTOS
+const listaProductos = [
+  almohadonesVarios,
+  setMesaYBancos,
+  cuadroVerde,
+  cuadroBlanco,
+  tallRibbedTower,
+  shortRibbedTower,
+]
+let card = document.getElementById("card")
+for(const producto of listaProductos){
+  let columna = document.createElement("div")
+  columna.className = "col-md-2 col-sm-12 mt-5"
+  columna.id = `columna-${producto.id}`
+  columna.innerHTML = 
+                `<div class="card">
+                  <img src=${producto.foto} style="width: 10rem; height: 10rem;" class="card-img-top" alt="almohadones varios">
+                  <div class="card-body">
+                    <h5 class="card-title">${producto.nombre} \n Codigo producto: ${producto.id}</h5>
+                    <p class="card-text">$${producto.precio}</p>
+                  </div>
+                </div>`
+  card.append(columna)
+}
+// FUNCION SEGUIR COMPRANDO
+function seguirComprando(){
+  let repregunta = prompt('¿Desea comprar algo mas? (si/no)')
+  let eleccion = repregunta.toUpperCase()
+  if(eleccion == 'SI'){
+      compra()
+  }else if(eleccion == 'NO'){
+      const total = totalCarrito()
+      totalCompra.innerHTML = `<p>El total de su compra es de: <b>$${total}</b></p>`
+  }else{
+      alert('Ingrese una opción valida')
+      seguirComprando()
+  }
+}
+//FUNCION TOTAL CARRITO
+function totalCarrito(){
+  return carrito.reduce((acc, el) => acc + el, 0)
+}
 // FUNCION COMPRA
 function compra(){
-    let pregunta = prompt(`Hola ${nombre} ¿Qué te gustaría comprar? \n 1- Deco \n 2- Cuadros \n 3- Velas`)
-    let eleccion = pregunta.toUpperCase()
-    if(eleccion == 1 || eleccion == 'DECO'){
-        let opciones = parseFloat(prompt(`Nuestras opciones son: \n 1. ${almohadonesVarios.nombre}: $${almohadonesVarios.precio} \n 2. ${setMesaYBancos.nombre}: $${setMesaYBancos.precio} \n`))
-        switch(opciones){
-            case 1:
-                card.innerHTML = 
-                `<img src="./img/almohadones2.jpeg" style="width: 18rem; height: 18rem;" class="card-img-top" alt="almohadones varios">
-                <div class="card-body">
-                  <h5 class="card-title">${almohadonesVarios.nombre}</h5>
-                  <p class="card-text">$${almohadonesVarios.precio}</p>
-                  <a href="#" class="btn btn-primary">Comprar</a>
-                </div>`
-                break
-            case 2:
-                card.innerHTML = 
-                `<img src="./img/mesaConBancos.jpeg" style="width: 18rem; height: 18rem; class="card-img-top" alt="mesa y bancos">
-                <div class="card-body">
-                  <h5 class="card-title">${setMesaYBancos.nombre}</h5>
-                  <p class="card-text">$${setMesaYBancos.precio}</p>
-                  <a href="#" class="btn btn-primary">Comprar</a>
-                </div>`
-                break
-            default:
-                alert("Opcion no valida")
-                compra()
-        }
-    }else if(eleccion == 2 || eleccion == 'CUADROS'){
-        let opciones = parseInt(prompt(`Nuestras opciones son: \n 1. ${cuadroVerde.nombre}: $${cuadroVerde.precio} \n 2. ${cuadroBlanco.nombre}: $${cuadroBlanco.precio}`))
-        switch(opciones){
-            case 1:
-                card.innerHTML = 
-                `<img src="./img/cuadro2.jpeg" style="width: 18rem; height: 18rem; class="card-img-top" alt="cuadro verde">
-                <div class="card-body">
-                  <h5 class="card-title">${cuadroVerde.nombre}</h5>
-                  <p class="card-text">$${cuadroVerde.precio}</p>
-                  <a href="#" class="btn btn-primary">Comprar</a>
-                </div>`
-                break
-            case 2:
-                card.innerHTML = 
-                `<img src="./img/comoda.jpeg" style="width: 18rem; height: 18rem; class="card-img-top" alt="cuadro blanco">
-                <div class="card-body">
-                  <h5 class="card-title">${
-                    cuadroBlanco.nombre}</h5>
-                  <p class="card-text">$${
-                    cuadroBlanco.precio}</p>
-                  <a href="#" class="btn btn-primary">Comprar</a>
-                </div>`
-                break
-            default: 
-                alert("Opcion no valida")
-                compra()
-        }
-    }else if(eleccion == 3 || eleccion == 'VELAS'){
-        let opciones = parseInt(prompt(`Nuestras opciones son: \n 1. ${tallRibbedTower.nombre}: $${tallRibbedTower.precio} \n 2. ${shortRibbedTower.nombre}: $${shortRibbedTower.precio}`))
-        switch(opciones){
-            case 1:
-                card.innerHTML = 
-                `<img src="./img/velas8.jpeg" style="width: 18rem; height: 18rem; class="card-img-top" alt="vela">
-                <div class="card-body">
-                  <h5 class="card-title">${tallRibbedTower.nombre}</h5>
-                  <p class="card-text">$${tallRibbedTower.precio}</p>
-                  <a href="#" class="btn btn-primary">Comprar</a>
-                </div>`
-                break
-            case 2:
-                card.innerHTML = 
-                `<img src="./img/velas12.jpeg" style="width: 18rem; height: 18rem; class="card-img-top" alt="vela">
-                <div class="card-body">
-                  <h5 class="card-title">${shortRibbedTower.nombre}</h5>
-                  <p class="card-text">$${shortRibbedTower.precio}</p>
-                  <a href="#" class="btn btn-primary">Comprar</a>
-                </div>`
-                break
-            default: 
-                alert("Opcion no valida")
-                compra()
-        }
-    }else{
-            alert('Ingrese un valor válido')
-            compra()
+    let codigo = parseInt(prompt(`Hola ${nombre} ingrese el codigo del producto que le gustaría comprar`))
+    if(codigo == 1){
+      carrito.push(almohadonesVarios.precio)
+      seguirComprando()
+    }else if(codigo == 2){
+      carrito.push(setMesaYBancos.precio)
+      seguirComprando()
+    }else if(codigo == 3){
+      carrito.push(cuadroVerde.precio)
+      seguirComprando()
+    }else if(codigo == 4){
+      carrito.push(cuadroBlanco.precio)
+      seguirComprando()
+    }else if(codigo == 5){
+      carrito.push(tallRibbedTower.precio)
+      seguirComprando()
+    }else if(codigo == 6){
+      carrito.push(shortRibbedTower.precio)
+      seguirComprando()
+    }
+    else{
+      alert('Ingrese un valor válido')
+      compra()
     }
 }
 compra()
